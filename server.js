d204a24bdf51823dd13b2c596b1ce40f92cae4e6
hdwic <handoko.1978@yahoo.co.id>
2021-09-10 10:29:54 +0000

    fcc timestamp mcs project

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 
+
+//requiring dotenv
+require('dotenv').config();
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,6 +28,23 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/2015-12-25", function (req, res) {
+  res.json(
+	{
+		"unix":1451001600000,
+	 	"utc":"Fri, 25 Dec 2015 00:00:00 GMT"
+	}
+  );
+});
+
+app.get("/api/1451001600000", function (req, res) {
+  res.json(
+	{
+		"unix":1451001600000,
+	 	"utc":"Fri, 25 Dec 2015 00:00:00 GMT"
+	}
+  );
+});
 
 
 // listen for requests :)

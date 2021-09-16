b13ae701876a0514f852fd261b5ce6007f723888
hdwic <handoko.1978@yahoo.co.id>
2021-09-16 08:06:46 +0000

    add datestring validator package

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var app = express();
 //requiring dotenv
 require('dotenv').config();
 
+//requiring iso-date-validator
+const {isValidDate} = require('iso-datestring-validator')
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -28,23 +32,94 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/2015-12-25", function (req, res) {
-  res.json(
-	{
-		"unix":1451001600000,
-	 	"utc":"Fri, 25 Dec 2015 00:00:00 GMT"
-	}
-  );
-});
 
-app.get("/api/1451001600000", function (req, res) {
-  res.json(
-	{
-		"unix":1451001600000,
-	 	"utc":"Fri, 25 Dec 2015 00:00:00 GMT"
-	}
-  );
-});
+app.get("/api/:date", function (req, res) {
+	
+	let input =  req.params.date.toString()
+	let output;
+	
+	const func = (input) => {
+    	// let output ;
+    	let dateMS = Date.parse(input) 
+    	let datecek1 = new Date(dateMS)
+    	let datecek2 = new Date(parseInt(input)) 
+    	let datecek3 = new Date(input)
+    	output = 
+    		{
+        		input : input, 
+        		dateMS : dateMS, 
+        		datecek1 : datecek1, 
+        		datecek2 : datecek2, 
+        		datecek3 : datecek3
+    		}
+    
+    		console.log('========================================')
+    		// console.log(`input : ${input}`)
+
+    		// console.log(`date milisecond: ${dateMS}`)
+    		// console.log(`date cek-1: ${datecek1}`)
+
+    		// console.log(`date cek-2: ${datecek2}`)
+    		// console.log(`date cek-3: ${datecek3}`)
+   		 // // console.log(`date another-2 = ${new Date(datecek2)}`)
+    		console.log(output)
+    		console.log('========================================')
+			console.log(`typeof output.dateMS : ${typeof output.dateMS}`)
+			console.log(`output.input.toString() : ${output.input.toString()} `)
+			console.log(`Date.parse(datecek2).toString() : ${Date.parse(datecek2).toString()}`)
+    		console.log('========================================')
+
+    return output
+
+}
+	func(req.params.date.toString())
+	
+		if (
+		isNaN(output.dateMS) && 
+		output.datecek1 == 'Invalid Date' && 
+		output.datecek2 == 'Invalid Date' && 
+		output.datecek3 == 'Invalid Date'
+		)
+			{
+				
+				res.json({
+					error : output.datecek1.toString()									  
+				})
+			}
+	
+	else  if (
+		// output.dateMS == 'Invalid Date' && 
+		// output.datecek1 == 'Invalid Date' && 
+		// output.datecek2 == 'Invalid Date' && 
+		// output.datecek3 == 'Invalid Date'
+		isValidDate(req.params.date.toString())
+		)
+			{
+				let dateMS = Date.parse(input) 
+    			let datecek1 = new Date(dateMS)
+				res.json({
+					unix : dateMS,
+					utc : datecek1.toString()
+									  
+				})
+			}
+	
+	 else if (
+		
+		!isValidDate(req.params.date.toString())
+		)
+			{
+				let datecek2 = new Date(parseInt(req.params.date.toString()))
+				res.json(
+					{
+						input : req.params.date.toString(),
+						 date : datecek2.toString()
+					}
+				)
+			}	
+
+})
+
 
 
 // listen for requests :)
